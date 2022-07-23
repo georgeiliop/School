ea5ae4c55b3f9ce2eb0328e2245f19f89cf0111b
GeorgeIliop <georgeiliopp@gmail.com>
2022-07-23 14:42:48 +0300

    added Graphics and OS1

---
+++ Operating systems I/2).sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+touch catalog.txt
+answ="yes" #answer
+while [[ $answ == "yes" ]]; do
+	echo "You have 6 options : 
+	type '-a' to add a new registration
+	type '-l' to show the contents of the list
+	type '-s' to sort the list
+	type '-c' to give a keyword
+	type '-d' to delete lines that contain a certain keyword
+	type '-n' to show the number of empty lines of the list"
+	read option
+	case $option in
+		-a) 
+			read -p "Give first name :" firstname
+			read -p "Give last name :" lastname
+			read -p "Give city :" city
+			read -p "Give number :" number
+			echo -e $firstname $lastname $city $number >> catalog.txt
+			;;
+		-l)
+			 grep . catalog.txt | cat -n 
+			;;
+		-s)
+			read sortline
+			sort -k $sortline catalog.txt
+			;;
+		-c)
+			read -p  "Give a keyword :" keyword
+			t1=$(grep $keyword /home/finixorigin/Documents/catalog.txt | wc -l)
+			if [ $t1 -eq  0 ]; then
+				echo There are no lines matching that keyword.
+			else
+				grep $keyword catalog.txt
+			fi
+			;;
+		-d)
+			read -p "Give a keyword :" key
+			read -p "Type -b if you want to add an empty line in the deleted ones , else type -r : " par
+			t3=$(grep $key /home/finixorigin/Documents/catalog.txt | wc -l) 
+			if [ $t3 -eq 0 ]; then
+				echo There are no lines matching that keyword.
+			else
+				if [[ "$par" == "-b" ]]; then
+					sed  's/.*'"$key"'.*//' catalog.txt
+					sed -i 's/.*'"$key"'.*//' catalog.txt
+				else
+					sed  '/'"$key"'/d' catalog.txt
+					sed -i '/'"$key"'/d' catalog.txt
+				fi
+			fi
+			;;
+		-n)
+			t2=$(grep -c "^$" /home/finixorigin/Documents/catalog.txt )
+			echo  "The number of empty lines is $t2 , do you want to delete them? ( yes/ no )"
+			read answer
+			if [[ $answer == "yes" ]]; then
+				sed -i '/^$/d' catalog.txt
+				echo They have been removed!
+			fi
+			;;
+
+		*)
+			echo "There is no such an option , please give one of the following ones :
+			type '-a' to add a new registration
+			type '-l' to show the contents of the list
+			type '-s' to sort the list
+			type '-c' to give a keyword
+			type '-d' to delete lines that contain a certain keyword
+			type '-n' to show the number of empty lines of the list"
+
+
+
+
+
+	esac
+	echo "Do you want to do something else? (yes/no) "
+	read answ
+done
---
+++ Operating systems I/searching.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+answer=y
+while [[ $answer = "y" ]]; do
+	t1=0
+	t2=0
+	t3=0
+	t4=0
+	t5=0
+	read -p "Give the name of the directory : " dirname
+	t1=$(find $dirname -type f -perm -$1| wc -l )
+	echo Found $t1 files that had those permissions :
+	find $dirname -type f -perm -$1
+
+	t2=$(find $dirname -type f -mmin -$2  | wc -l)
+	echo Found $t2 files that were modified the last $2 days :
+	find $dirname -type f -mmin -$2 
+
+	t3=$(find $dirname -type d -atime -$2 | wc -l)
+	echo Found $t3 directories that were accessed the last $2 days :
+	find $dirname -type d -atime -$2
+
+	t4=$(find $dirname -type s -type p | wc -l)
+	echo Found $t4 file type socket or pipe : 
+	find $dirname -type s -type p 
+	t5=$(find $dirname -type f -empty | wc -l)
+	echo Found $t5 empty files :
+	find $dirname -type f -empty 
+	echo " Do you want to give onother directory (y/n) ? "
+	read answer
+	let t=t+t1+t2+t3+t4+t5
+
+
+
+done
+echo The sum of the files and directories found is $t
+
+
